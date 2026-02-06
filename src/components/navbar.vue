5aebdef6774f33b89fb4520fd45ef5ff226a2e54
ajs <140010746+aboodJS@users.noreply.github.com>
2026-02-06 17:41:06 +0300

    added some simple pages and components

---
+++ src/components/navbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <ul>
+      <RouterLink to="/">home</RouterLink>
+      <RouterLink to="/about">about</RouterLink>
+    </ul>
+  </nav>
+</template>
---
+++ src/views/Index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Navbar from "@/components/navbar.vue";
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <h1>Hello world</h1>
+</template>
